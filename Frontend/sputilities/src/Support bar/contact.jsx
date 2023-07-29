722999a73c640368557c745437900cde51d43bb5
Puneeth <pyd773@gmail.com>
2023-07-29 19:45:14 +0530

    Feature 18: Adding links to Support bar at the last of our web page

--- Frontend/sputilities/src/Support bar/contact.jsx
+++ Frontend/sputilities/src/Support bar/contact.jsx
@@ -6,14 +6,20 @@ const Contact = () => {
       <div>
         <h3 className="contact">Contact us</h3>
         <div className="footer-icons">
-          <a href="#" className="footer-icon">
+          <a href="mailto:pyd773@gmail.com" className="footer-icon">
             <i className="fa-solid fa-envelope"></i>
             <span className="message">Mail</span>
           </a>
-          <a href="#" className="footer-icon">
+          <a
+            href="https://github.com/puneeth072003/sputilities.V1"
+            className="footer-icon"
+          >
             <i className="fa-brands fa-github"></i>
           </a>
-          <a href="#" className="footer-icon">
+          <a
+            href="https://github.com/puneeth072003/sputilities.V1/issues/new"
+            className="footer-icon"
+          >
             <i className="fa-solid fa-bug"></i>
           </a>
         </div>
