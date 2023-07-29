685f89da361f29edf64add437dfe32d819564c9a
Puneeth <pyd773@gmail.com>
2023-07-29 21:35:21 +0530

    Feature 20: Completing steops section

--- Frontend/sputilities/src/App/Steps.jsx
+++ Frontend/sputilities/src/App/Steps.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 
+const step1 = `Click on the "Login" button in the top right corner of the webpage. You will be redirected to Spotify's login page. Enter your Spotify username and password to log in.`;
+const step2 = `After logging in, you'll see a screen requesting your permission to authorize Sputilities to access some of your Spotify data. Read the permissions carefully and click "Agree" to authorize Sputilities.`;
+const step3 = `Once you've authorized Sputilities, you'll be redirected back to the Sputilities website. Congratulations! You're now logged in and authorized to use Sputilities. You can now explore and use the features provided by Sputilities.`;
 const Steps = () => {
   return (
     <div className="cont-app">
       <h2 className="head-steps">How to use sputilities?</h2>
       <div className="steps">
-        <p>
+        <p className="step-dis">
           <h3 className="step-head">step 1</h3>
-          Paragraph 1
+          {step1}
         </p>
-        <p>
+        <p className="step-dis">
           <h3 className="step-head">step 2</h3>
-          Paragraph 1
+          {step2}
         </p>
-        <p>
+        <p className="step-dis">
           <h3 className="step-head">step 3</h3>
-          Paragraph 1
+          {step3}
         </p>
       </div>
     </div>
