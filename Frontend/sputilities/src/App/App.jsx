4ce64634cb0834fe47b08a20bcb7d32bf052de2b
Puneeth <pyd773@gmail.com>
2023-07-29 20:09:09 +0530

    Feature 19: Adding basic steps to follow

--- Frontend/sputilities/src/App/App.jsx
+++ Frontend/sputilities/src/App/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ReactDOM } from "react";
 import "./App.css";
 import Feature_1 from "./assets/feature1.webp";
+import { Steps } from "./Steps";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <div>
         <img src={Feature_1} className="fet-1-img" alt="Image" />
       </div>
+      <Steps />
     </div>
   );
 };
---
+++ Frontend/sputilities/src/App/Steps.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Steps = () => {
+  return (
+    <div className="cont-app">
+      <h2 className="head-steps">How to use sputilities?</h2>
+      <div className="steps">
+        <p>
+          <h3 className="step-head">step 1</h3>
+          Paragraph 1
+        </p>
+        <p>
+          <h3 className="step-head">step 2</h3>
+          Paragraph 1
+        </p>
+        <p>
+          <h3 className="step-head">step 3</h3>
+          Paragraph 1
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export { Steps };
