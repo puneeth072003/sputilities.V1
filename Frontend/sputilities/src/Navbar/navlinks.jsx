0878927359a9b53ea822f1d59f3cd4ffafc65d7c
Puneeth Y <pyd773@gmail.com>
2025-03-23 20:16:37 +0530

    Feat: Add Bootstrap for styling and implement toast notifications in Navbar

--- Frontend/sputilities/src/Navbar/navlinks.jsx
+++ Frontend/sputilities/src/Navbar/navlinks.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.css";
-import { fetchProfile } from "../Controllers/login";
 import axios from "axios";
 
 const Navlinks = () => {
   const url = "http://localhost:3600/api/v1/login";
   const [auth, setAuth] = useState(false);
+  const [showToast, setShowToast] = useState(false);
 
-  const handleLogin = async () => {
+  const handleLogin = () => {
     try {
-      const response = await axios.get(url);
-      if (response.data.redirectUrl) {
-        window.location.href = response.data.redirectUrl;
-      }
+      // await axios.get(url);
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 3000); 
     } catch (error) {
       console.error("Error in login:", error);
     }
   };
-  
+
   const fetchUsername = async () => {
     try {
       const username = await axios.get();
@@ -27,7 +25,7 @@ const Navlinks = () => {
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetchUsername();
   }, []);
 
@@ -38,6 +36,17 @@ const Navlinks = () => {
           <button onClick={handleLogin} className="login-btn">
             {auth ? `${window.name}` : "Login"}
           </button>
+          {showToast && (
+            <div className="toast show">
+              <div className="toast-header">
+                <strong className="me-auto">Notification</strong>
+                <button type="button" className="btn-close" onClick={() => setShowToast(false)} aria-label="Close"></button>
+              </div>
+              <div className="toast-body">
+              Oops! Our backend is currently on an unscheduled vacation. Logging in isn’t possible at the moment; thanks for your understanding!
+              </div>
+            </div>
+          )}
         </li>
       </ul>
     </nav>
--- Frontend/sputilities/src/main.jsx
+++ Frontend/sputilities/src/main.jsx
@@ -10,6 +10,9 @@ import { Support } from "./Support bar/Support.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div>
     <Navbar />
+    <div className="marquee">
+      <p className="marquee-p">Due to cloud cost concerns, we’ve decided not to deploy the backend; apparently, even our cloud has a budget! We truly appreciate your understanding and support, even if it means some features are on an unexpected vacation.</p>
+    </div>    
     <BrowserRouter>
       <Content />
     </BrowserRouter>
